8ba5fc27c726ad7f56c1ce9c751c0d5fa73914f9
Kiana200 <kianaandarzig@gmail.com>
2025-05-20 17:14:23 +0200

    "Update ontologies"

--- python_app/meta_data.py
+++ python_app/meta_data.py
@@ -1,14 +1,21 @@
 POD_METADATA = {
+    "solid_pod_0": {
+        "name": "Construction",
+        "description": "Provides the general Construction ontology, including classes for Products, Components, InventoryItems, StorageLocations and Assessments.",
+        "ontology_url": "http://solid-pod-0/ontology#",
+        "data_url": "http://solid-pod-0/data/",
+        "pod_url": "http://localhost:3030/solid-pod-0/sparql"
+    },
     "solid_pod_1": {
         "name": "Lindner",
-        "description": "Contains product/component data and related Ontology associated with products and components.",
+        "description": "Holds Lindner-specific product and component classes and data, extending the general Construction ontology with company-specific details.",
         "ontology_url": "http://solid-pod-1/ontology#",
         "data_url": "http://solid-pod-1/data/",
         "pod_url": "http://localhost:3030/solid-pod-1/sparql"
     },
     "solid_pod_2": {
         "name": "Concular",
-        "description": "Contains inventory and assessment of an inventory item with the associated Ontology.",
+        "description": "Contains Concular-specific inventory items, storage locations and assessments, all as specializations of the general Construction ontology.",
         "ontology_url": "http://solid-pod-2/ontology#",
         "data_url": "http://solid-pod-2/data/",
         "pod_url": "http://localhost:3030/solid-pod-2/sparql"
